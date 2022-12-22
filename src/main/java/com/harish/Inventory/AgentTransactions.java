11bf53bfa3c84111b2b6ae25ac6ffe34d6cd6afe
harish <harishvk1018@gmail.com>
2022-12-22 16:13:07 +0530

    Final

---
+++ src/main/java/com/harish/Inventory/AgentTransactions.java
@@ -0,0 +1,95 @@
+package com.harish.Inventory;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="agent_transaction")
+
+public class AgentTransactions implements Serializable
+{
+	@Id
+	@Column(name="id")
+	@GenericGenerator(name = "b_info", strategy = "increment")
+	@GeneratedValue(generator = "b_info")
+	private int id;
+
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="quantity")
+	private int quantity;
+	
+	@Column(name="price")
+	private double price;
+	
+	@Column(name="date")
+	private String date;
+	
+	@Column(name="agent_id")
+	private int agentId;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getAgentId() {
+		return agentId;
+	}
+
+	public void setAgentId(int agentId) {
+		this.agentId = agentId;
+	}
+
+	@Override
+	public String toString() {
+		return "AgentTransactions [id=" + id + ", name=" + name + ", quantity=" + quantity + ", price=" + price
+				+ ", date=" + date + ", agentId=" + agentId + "]";
+	}
+
+	
+
+}
